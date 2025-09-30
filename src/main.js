7104f22485b7dc54fa0b0e1b590eb3f73b2b9351
DmytroPryadko <160414230+DmytroPryadko@users.noreply.github.com>
2025-09-30 20:09:44 +0200

    done1

--- src/main.js
+++ src/main.js
@@ -1,65 +1,39 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import { renderCard, refs, handlerError } from './js/render-function';
+import { fetchImage, generateSearchString } from './js/paxabay-api';
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import { createMarkup } from "./js/render-functions.js";
-import { getInform } from "./js/pixabay-api.js";
+refs.searchForm.addEventListener('submit', handlerSearchButton);
 
-const form = document.querySelector(".form-inline");
-const list = document.querySelector(".js-list");
-const loader = document.querySelector(".loader");
-
-form.addEventListener("submit", searchImages);
-
-function loaderShow() {
-    loader.classList.toggle("visible");
-}
-
-const lightbox = new SimpleLightbox('.images a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-});
-
-
-function searchImages(evt) {
-    evt.preventDefault();
-    list.innerHTML = '';
-    const { query } = evt.currentTarget.elements;
-    let searchInput = query.value.trim();
-    if (searchInput === '') {
-        iziToast.error({
-            title: 'Error',
-            message: 'The field cannot be empty!!!',
-            position: 'topRight',
-        });
-        form.reset();
+function handlerSearchButton(event) {
+  event.preventDefault();
+  const searchText = event.target.searchtext.value;
+  if (!searchText) {
+    handlerError('outdata');
+    return;
+  }
+  refs.gallery.innerHTML = '';
+  refs.loader.classList.add('loader');
+  fetchImage(generateSearchString(searchText))
+    .then(image => {
+      refs.loader.classList.remove('loader');
+      if (image.totalHits === 0) {
+        handlerError('nodata');
         return;
-    }
-
-    loaderShow();
-
-    getInform(searchInput)
-        .then(data => {
-            if (data.hits.length === 0) {
-                iziToast.warning({
-                    title: '',
-                    message: 'Sorry, there are no images matching your search query. Please try again!',
-                    position: 'topRight',
-                });
-            } else {
-                list.insertAdjacentHTML("beforeend", createMarkup(data.hits));
-                lightbox.refresh();
-            }
-        })
-        .catch(error => {
-            iziToast.error({
-                title: 'Error',
-                message: 'An error occurred while fetching data. Please try again later.',
-                position: 'topRight',
-            });
-        })
-        .finally(() => loaderShow());
-    form.reset();
+      }
+      refs.gallery.insertAdjacentHTML('beforeend', renderCard(image.hits));
+      galleryBigImage.refresh();
+    })
+    .catch(error => {
+      refs.loader.classList.remove('loader');
+      handlerError(error);
+    })
+    .finally(refs.searchForm.reset());
 }
+const galleryBigImage = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  overlayOpacity: 0.8,
+  scrollZoom: false,
+});
+galleryBigImage.on('show.simplelightbox', function () {});
