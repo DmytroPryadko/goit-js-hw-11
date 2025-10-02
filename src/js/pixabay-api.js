92a3b02cfd91f1c2f87b3e3ab88de120bc10f6e3
DmytroPryadko <160414230+DmytroPryadko@users.noreply.github.com>
2025-10-02 17:55:06 +0200

    done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,3 @@
-
-
-
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = "44781960-4338c4c6360104e4623fadff2";
-const imageType = 'photo';
-const orientation = 'horizontal';
-const safeSearch = true;
-
-export function getPictures(query) {
-    return fetch(`${BASE_URL}?key=${API_KEY}&q=${query}&image_type=${imageType}&orientation=${orientation}&safesearch=${safeSearch}`)
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error(res.status);
-            }
-            return res.json();
-        });
+export function httpRequest(key, searchText) {
+  return  fetch( `https://pixabay.com/api/?key=${key}&q=${searchText}&image_type=photo&orientation=horisontal&safesearch=true`)
 }
